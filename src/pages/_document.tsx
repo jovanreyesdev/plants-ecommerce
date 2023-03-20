36b91edbedc3604ddcb34f8b11dcdd6be49ee961
Jovan <jovan.reyes.dev@gmail.com>
2023-03-20 18:23:09 +0800

    update page metadata

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -3,7 +3,24 @@ import { Html, Head, Main, NextScript } from 'next/document'
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <meta name="description" content="Discover the Plant Store, your ultimate destination for purchasing high-quality plants. Browse our extensive selection and enjoy a secure checkout process. Contact us at jovan.reyes.dev@gmail.com for any questions or concerns." />
+
+        <meta itemprop="name" content="Plant Store" />
+        <meta itemprop="description" content="Discover the Plant Store, your ultimate destination for purchasing high-quality plants. Browse our extensive selection and enjoy a secure checkout process. Contact us at jovan.reyes.dev@gmail.com for any questions or concerns." />
+        <meta itemprop="image" content="/thumbnail.jpg" />
+
+        <meta property="og:url" content="https://jovanreyesdev.netlify.app" />
+        <meta property="og:type" content="website" />
+        <meta property="og:title" content="Plant Store" />
+        <meta property="og:description" content="Discover the Plant Store, your ultimate destination for purchasing high-quality plants. Browse our extensive selection and enjoy a secure checkout process. Contact us at jovan.reyes.dev@gmail.com for any questions or concerns." />
+        <meta property="og:image" content="/thumbnail.jpg" />
+
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta name="twitter:title" content="Plant Store" />
+        <meta name="twitter:description" content="Discover the Plant Store, your ultimate destination for purchasing high-quality plants. Browse our extensive selection and enjoy a secure checkout process. Contact us at jovan.reyes.dev@gmail.com for any questions or concerns." />
+        <meta name="twitter:image" content="/thumbnail.jpg" />
+      </Head>
       <body className="font-sans">
         <Main />
         <NextScript />
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -14,7 +14,6 @@ export default function Home() {
     <>
       <Head>
         <title>Plant Store</title>
-        <meta name="description" content="Discover the Plant Store, your ultimate destination for purchasing high-quality plants. Browse our extensive selection and enjoy a secure checkout process. Contact us at jovan.reyes.dev@gmail.com for any questions or concerns." />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
